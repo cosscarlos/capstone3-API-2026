cda22300d6c3ceb289b1da1fc757b24be69853b0
carloscoss <crobertohenriquez@gmail.com>
2026-06-24 16:09:52 -0500

    Phase 4
    Updates:
    
    - Setting the getByUserId adding update methods.

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -1,10 +1,12 @@
 package org.yearup.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
 @Service
+@Transactional
 public class ProfileService
 {
     private final ProfileRepository profileRepository;
@@ -14,8 +16,17 @@ public class ProfileService
         this.profileRepository = profileRepository;
     }
 
+    public Profile getByUserId(int userId){
+        return profileRepository.findById(userId).orElse(null);
+    }
+
     public Profile create(Profile profile)
     {
+        return profileRepository.save(profile);
+    }
+
+    public Profile update(int userId, Profile profile){
+        profile.setUserId(userId);
         return profileRepository.save(profile);
     }
 }
